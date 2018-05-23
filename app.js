a6811ba09e24ac9455399353df9c7779c0057a4f
Esterling Accime <esterling@brainbytescreative.com>
2018-05-23 00:38:15 -0400

    fix: Environment Variable Youtube Video

---
+++ app.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const password = process.env.PASSWORD;
+
+const secret = process.env.PASSWORD;
+
+console.log("Password: ", password);
+console.log("Secret: ", secret);
---
+++ notes.js
@@ -0,0 +1,8 @@
+// Step 1
+// MYAPIKEY=ndsvn2g8dnsb9hsg node server.js
+
+// Step 2
+// export MYAPIKEY=ndsvn2g8dnsb9hsg
+
+// Step 3
+// the dotenv package
